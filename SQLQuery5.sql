321f1fd13269266c358b22052cc4ed0fe042e2a1
Raymond Nguyen <137119948+RaymondTriNguyen@users.noreply.github.com>
2025-08-07 19:58:00 -0500

    Add files via upload

--- SQLQuery5.sql
+++ SQLQuery5.sql
@@ -8,3 +8,13 @@ select Worker, Total from Table1 where Total>1000;
 select Worker, Price from Table1 order by Price desc;
 --V) What is the average hourly rate of all workers?--
 select avg (Price) as AveragePrice from Table1;
+--Find the worker with the lowest hourly rate--
+select top 1 Worker, Price from Table1 order by Price asc;
+--List all workers who aren't between 800 dollars to 1200 dollars.--
+select Worker, Total from Table1 where Total between 800 and 1200;
+--Show workers' name in alphabetical order--
+select Worker from Table1 order by Worker  
+--Get the worker's name who worked exactly 24 hours
+select Worker, Hours from Table1 where Hours=24;
+--Calculate total pay for workers with more than 20 hours--
+select sum (Total) as Total from Table1 where Hours>20 ;
